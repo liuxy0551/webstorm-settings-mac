86d14ee9da632987b8138aede1713c89f1335ee6
liuyi <liuxy0551@qq.com>
2021-03-02 10:04:12 +0800

    WS-2019.3.1 <liuyi@Liuyi.local Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, Vue Single File Component.vue, ide.general.xml, web-types-registry.xml, monokai.icls, git_toolbox_2.xml, project.default.xml, diff.xml, user.xml, git.xml, Zen CSS.xml, Default.xml, HTML File.html, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, notifications.xml, Dart.xml, path.macros.xml, wechatCode.xml, github.xml, templates.xml, Obsidian.icls, customization.xml, React.xml, Default.xml, JavaScript.xml, phonegap.xml, laf.xml, web-browsers.xml, vcs.xml, keymap.xml, editor.xml, usageView.xml, debugger.xml, Mac OS X copy.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,21 @@
+<!-- Created by liuyi on ${YEAR}-${MONTH}-${DAY} ${TIME}:${SECOND} -->
+<template>
+  <div>
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "${COMPONENT_NAME}",
+    data () {
+      return {
+      
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
